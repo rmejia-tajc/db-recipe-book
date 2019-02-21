c8af2abae49e2e6e9cbd7644aaca84dde94f3ce5
rmejia-tajc <rmejia.tajc@gmail.com>
2019-02-21 15:17:45 -0700

    create seeds

---
+++ seeds/003_recipesIngredients.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('recipesIngredients')
+    .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipesIngredients').insert([
+        { recipe_id: '1', ingredient_id: '1', amount: '1'},
+        { recipe_id: '1', ingredient_id: '2', amount: '1'},
+        { recipe_id: '1', ingredient_id: '3', amount: '4'},
+        { recipe_id: '2', ingredient_id: '1', amount: '2'},
+        { recipe_id: '2', ingredient_id: '2', amount: '0.5'},
+        { recipe_id: '2', ingredient_id: '3', amount: '2'},
+        { recipe_id: '3', ingredient_id: '1', amount: '4'},
+        { recipe_id: '3', ingredient_id: '4', amount: '3'}
+      ]);
+    });
+};
